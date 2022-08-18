3cce734c298eaec258f0e3c48bd205fc69c7d10e
dafiwi <94173804+dafiwi@users.noreply.github.com>
2022-08-18 11:48:52 +0200

    Add files via upload

---
+++ TimeZoneOverview_Console/Source_Files/Models/TimeZone.cs
@@ -0,0 +1,14 @@
+﻿namespace TimeZoneOverview.Models
+{
+    internal class TimeZone
+    {
+        public string TimeZoneName;
+        public string TimeZoneTime;
+
+        public TimeZone(string timeZoneName, string timeZoneTime)
+        {
+            TimeZoneName = timeZoneName;
+            TimeZoneTime = timeZoneTime;
+        }
+    }
+}
---
+++ TimeZoneOverview_Console/Source_Files/Models/TimeZoneListEntry.cs
@@ -0,0 +1,19 @@
+﻿namespace TimeZoneOverview.Models
+{
+    internal class TimeZoneListEntry
+    {
+        public string TimeZoneStandardName;
+        public string TimeZoneBaseUtcOffset;
+
+        public TimeZoneListEntry(string timeZoneStandardName, string timeZoneBaseUtcOffset)
+        {
+            TimeZoneStandardName = timeZoneStandardName;
+            TimeZoneBaseUtcOffset = timeZoneBaseUtcOffset;
+        }
+
+        public override string ToString()
+        { 
+            return TimeZoneStandardName + "\t" + TimeZoneBaseUtcOffset;
+        }
+    }
+}
